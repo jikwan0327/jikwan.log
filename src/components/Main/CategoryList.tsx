32d0d31d100edef6db7d85996df3be4373d05188
jikwan0327 <jikwan0327@gmail.com>
2024-09-23 23:51:02 +0900

    feat: 푸터, 카테고리 테스트

---
+++ src/components/Main/CategoryList.tsx
@@ -0,0 +1,62 @@
+import React, { ReactNode } from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'gatsby'
+
+export type CategoryListProps = {
+  selectedCategory: string
+  categoryList: {
+    [key: string]: number
+  }
+}
+
+type CategoryItemProps = {
+  active: boolean
+}
+
+type GatsbyLinkProps = {
+  children: ReactNode
+  className?: string
+  to: string
+} & CategoryItemProps
+
+const CategoryListWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 768px;
+  margin: 100px auto 0;
+`
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const CategoryItem = styled(({ active, ...props }: GatsbyLinkProps) => (
+  <Link {...props} />
+))<CategoryItemProps>`
+  margin-right: 20px;
+  padding: 5px 0;
+  font-size: 18px;
+  font-weight: ${({ active }) => (active ? '800' : '400')};
+  cursor: pointer;
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+`
+
+const CategoryList = ({
+  selectedCategory,
+  categoryList,
+}: CategoryListProps) => {
+  return (
+    <CategoryListWrapper>
+      {Object.entries(categoryList).map(([name, count]) => (
+        <CategoryItem
+          to={`/?category=${name}`}
+          active={name === selectedCategory}
+          key={name}
+        >
+          #{name}({count})
+        </CategoryItem>
+      ))}
+    </CategoryListWrapper>
+  )
+}
+
+export default CategoryList
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
+<<<<<<< HEAD
 import React from 'react'
 import styled from '@emotion/styled'
 import GlobalStyle from 'components/Common/GlobalStyle'
 import Introduction from 'components/Main/Introduction'
+import Footer from 'components/Common/Footer'
+import CategoryList from 'components/Main/CategoryList'
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+}
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 100vh;
 `
 
 const IndexPage = function () {
@@ -14,7 +23,21 @@ const IndexPage = function () {
     <Container>
       <GlobalStyle />
       <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
+      <Footer />
     </Container>
+=======
+import React, { FunctionComponent } from 'react'
+import Text from 'components/Text'
+import { Link } from 'gatsby'
+
+const IndexPage: FunctionComponent = function () {
+  return (
+    <div>
+      <Text text="Home" />
+      <Link to="/info">To Info</Link>
+    </div>
+>>>>>>> parent of 262e4fb (feat: 페이지 상단 테스트)
   )
 }
 
