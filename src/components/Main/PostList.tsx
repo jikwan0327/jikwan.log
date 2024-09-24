24806494b7acf5e958506e68ee200f8f40108e76
jikwan0327 <jikwan0327@gmail.com>
2024-09-24 23:50:13 +0900

    feat: 게시글 목록 테스트

---
+++ src/components/Main/PostList.tsx
@@ -0,0 +1,36 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import PostItem from 'components/Main/PostItem'
+
+const POST_ITEM_DATA = {
+  title: 'Post Item Title',
+  date: '2020.01.29.',
+  categories: ['Web', 'Frontend', 'Testing'],
+  summary:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
+  thumbnail:
+    '<https://ji5485.github.io/static/e4f34c558ae8e8235ff53b0311085796/4d854/javascript-core-concept-summary-function-1.webp>',
+  link: '<https://www.google.co.kr/>',
+}
+
+const PostListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  width: 768px;
+  margin: 0 auto;
+  padding: 50px 0 100px;
+`
+
+const PostList: FunctionComponent = function () {
+  return (
+    <PostListWrapper>
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+    </PostListWrapper>
+  )
+}
+
+export default PostList
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
 import React from 'react'
 import styled from '@emotion/styled'
 import GlobalStyle from 'components/Common/GlobalStyle'
 import Introduction from 'components/Main/Introduction'
 import Footer from 'components/Common/Footer'
 import CategoryList from 'components/Main/CategoryList'
+import PostList from 'components/Main/PostList'
 
 const CATEGORY_LIST = {
   All: 5,
@@ -25,19 +25,8 @@ const IndexPage = function () {
       <Introduction />
       <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
       <Footer />
+      <PostList />
     </Container>
-=======
-import React, { FunctionComponent } from 'react'
-import Text from 'components/Text'
-import { Link } from 'gatsby'
-
-const IndexPage: FunctionComponent = function () {
-  return (
-    <div>
-      <Text text="Home" />
-      <Link to="/info">To Info</Link>
-    </div>
->>>>>>> parent of 262e4fb (feat: 페이지 상단 테스트)
   )
 }
 
